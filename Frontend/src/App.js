2e8198168d9bd5d760837a9ce646be86a3ef4df0
Kavindu-Hasintha <mkavinduh@gmail.com>
2023-09-29 14:35:09 +0530

    Add error handling part for movies get API

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,21 +6,56 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMovieHandler() {
     setIsLoading(true);
-    const response = await fetch("https://localhost:7284/movie");
-    const data = await response.json();
-    const transformedMovies = data.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_Crawl,
-        releaseData: movieData.release_Date,
-      };
-    });
-    setMovies(transformedMovies);
-    setIsLoading(false);
+    setError(null);
+
+    try {
+      const response = await fetch("https://localhost:7284/movie");
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_Crawl,
+          releaseData: movieData.release_Date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  let content = <p>Found no movies!</p>;
+
+  if (movies.length === 0) {
+    content = <p>Found no movies!</p>;
+  }
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = (
+      <div className="spinner-border text-primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
   }
 
   return (
@@ -28,15 +63,7 @@ function App() {
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {isLoading && (
-          <div className="spinner-border text-primary" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </div>
-        )}
-        {!isLoading && movies.length === 0 && <p>Fount no movies!</p>}
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
