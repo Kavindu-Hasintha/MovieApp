a98fe49ee7369ce48ae177e875b7c65043fe0b5d
Kavindu-Hasintha <mkavinduh@gmail.com>
2023-09-29 13:42:02 +0530

    Add loading part

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,32 +5,22 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMovieHandler() {
-    // Default method of fetch is GET
-    // Sending a HTTP request is an asynchronous task
-    // It doesn't finish immediately
-    // So we use then, it means when we get a response
-    // catch to handle any errors
-    // response in then - it is an object with the bunch of data about the response
-    // response.json() - automatically translate JSON response body to a real JS object
-    // data - earlier data transformation is done
+  const [isLoading, setIsLoading] = useState(false);
 
-    fetch("https://localhost:7284/movie")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        const transformedMovies = data.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_Crawl,
-            releaseData: movieData.release_Date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMovieHandler() {
+    setIsLoading(true);
+    const response = await fetch("https://localhost:7284/movie");
+    const data = await response.json();
+    const transformedMovies = data.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_Crawl,
+        releaseData: movieData.release_Date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -39,7 +29,13 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && (
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        )}
+        {!isLoading && movies.length === 0 && <p>Fount no movies!</p>}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
